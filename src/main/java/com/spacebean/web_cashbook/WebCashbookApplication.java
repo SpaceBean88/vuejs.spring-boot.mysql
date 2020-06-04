bb5a11b4c0bc5c187545f4d498d7f6120f33fe60
SpaceBean88 <soneju88@gmail.com>
2020-06-04 12:11:33 +0900

    create back-end scaffold

---
+++ src/main/java/com/spacebean/web_cashbook/WebCashbookApplication.java
@@ -0,0 +1,13 @@
+package com.spacebean.web_cashbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCashbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCashbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spacebean/web_cashbook/WebCashbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacebean.web_cashbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCashbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
